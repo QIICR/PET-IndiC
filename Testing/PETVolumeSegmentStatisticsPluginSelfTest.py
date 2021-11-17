d4ecadcac97846e3d700c6607aef2c36912803ba
Christian Bauer <christian-bauer@uiowa.edu>
2021-11-17 12:17:44 -0600

    COMP: test data moved to different server
    Server `http://slicer.kitware.com` is no longer online; see `Slicer/Slicer#5797
    Moved test data from `http://slicer.kitware.com/midas3/download/item/257234/QIN-HEADNECK-01-0139-PET.zip`
    to `https://github.com/QIICR/PETTumorSegmentation/releases/download/4.10.2/QIN-HEADNECK-01-0139-PET.zip`

--- Testing/PETVolumeSegmentStatisticsPluginSelfTest.py
+++ Testing/PETVolumeSegmentStatisticsPluginSelfTest.py
@@ -98,7 +98,7 @@ class PETVolumeSegmentStatisticsPluginSelfTestTest(ScriptedLoadableModuleTest):
   def loadTestData(self):
     self.patienName = 'QIN-HEADNECK-01-0139'
     #download data and add to dicom database
-    zipFileUrl = 'http://slicer.kitware.com/midas3/download/item/257234/QIN-HEADNECK-01-0139-PET.zip'
+    zipFileUrl = 'http://github.com/QIICR/PETTumorSegmentation/releases/download/4.10.2/QIN-HEADNECK-01-0139-PET.zip'
     zipFilePath = self.tempDataDir+'/dicom.zip'
     zipFileData = self.tempDataDir+'/dicom'
     expectedNumOfFiles = 545
